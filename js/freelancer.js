b1d413cee221c2976ddaefe80a4f4ee67e00b79f
Alexey Aristov <aav@acm.org>
2016-01-17 20:50:57 +0100

    initial

---
+++ js/freelancer.js
@@ -0,0 +1,108 @@
+/* ===================================================
+ * template specific javascript funtions
+ * ================================================ */
+
+
+function init_masonry(){
+    
+    var $container = $('#portfolio');
+    var gutter = 12;
+    var min_width = 150;
+
+    $container.imagesLoaded( function(){
+        
+        $container.masonry({
+
+            itemSelector: '.portfolio-item',
+            gutterWidth: gutter,
+            isAnimated: true,
+            columnWidth: function( containerWidth ) {
+
+                var num_of_boxes = (containerWidth/min_width | 0);
+
+                var box_width = (((containerWidth - (num_of_boxes-1)*gutter)/num_of_boxes) | 0) ;
+
+                if (containerWidth < min_width) {
+                    box_width = containerWidth;
+                }
+
+                $('.portfolio-item').width(box_width);
+
+                return box_width;
+            }
+        });
+    });
+}
+
+$('a[href="#portfolio-tab"]').on('shown', function (e){
+
+    init_masonry();
+
+    window.setTimeout(function() {
+
+        $(window).trigger('resize');
+    
+    }, 1000);
+
+});
+
+/* google maps */
+var myLatlng = new google.maps.LatLng(48.7218435,9.270561);
+
+var mapInitOpts = {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: myLatlng,
+    zoom: 12
+};
+
+var map = new google.maps.Map(document.getElementById("my-location"), mapInitOpts);
+
+var marker = new google.maps.Marker({
+    position: myLatlng, 
+    map: map
+});
+
+
+$('a[href="#contact-tab"]').on('shown', function (e){
+
+    center_map();
+
+    google.maps.event.trigger(map, 'resize'); 
+    moveTo();
+
+});
+
+$(window).on('resize', function (e){
+    center_map();
+});
+
+function center_map(){
+    window.setTimeout(function() {
+        map.panTo(marker.getPosition()); 
+        }, 500);
+};
+
+$(document).ready(function() {
+    $("h4.my-skill").each(function(ix) {
+         var host = $(this);
+         var span = $(document.createElement("span"));
+
+         host.append(span);
+
+         var rank = 0;
+
+         var data_rank = host.data("rank");
+         if(data_rank) 
+            rank = parseInt(data_rank);
+
+         for(n=1; n<=5; n++) {
+                var star = $(document.createElement("i"));
+
+                star.addClass(n <= rank ? "icon-star" : "icon-star-empty");
+                span.append(star);
+         }
+    });
+});
+
+
+
